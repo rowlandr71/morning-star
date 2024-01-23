e687c45e94e40e6c1ff2bd92ea2bd25cbc531370
joeyomi <joseph.oyomi11@gmail.com>
2024-01-23 23:41:01 +0100

    feat: ecr repositories

---
+++ terraform/ecr.tf
@@ -0,0 +1,35 @@
+#------------------------------------------------------------------------------
+# ECR Repository
+#------------------------------------------------------------------------------
+module "ecr_repository" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "~> 1.0"
+
+  repository_name = "${local.name}-timeoff"
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn, ]
+  repository_read_access_arns       = ["*"]
+  create_lifecycle_policy           = true
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  repository_force_delete         = true
+  repository_image_tag_mutability = "MUTABLE"
+
+  tags = local.tags
+}
